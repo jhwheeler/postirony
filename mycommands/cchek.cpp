c7160f45b9a879e81d4d237bf4ea004bd711f236
archqua <zakharovpnsd@mail.ru>
2020-04-21 19:55:27 +0300

    added several commands and descripion

---
+++ mycommands/cchek.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 10; ++i) {cout << i << endl;}
+  return 0;
+}
